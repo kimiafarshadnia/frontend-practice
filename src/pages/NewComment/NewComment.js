c30ba7e8781bea9abe8ea1257ebb7d942cbd5bb7
kimia <farshadniakimia@gmail.com>
2023-01-24 16:35:17 +0330

    add new class style

--- src/pages/NewComment/NewComment.js
+++ src/pages/NewComment/NewComment.js
@@ -35,30 +35,30 @@ const NewComment = ({ history }) => {
       <div className="flex flex-col md:flex-row justify-center items-center w-full">
         <div className="flex flex-col mb-2 w-full md:w-2/4 md:mr-4">
           <label className="capitalize font-medium mr-3">name</label>
-          <input type="text" className="p-1 sm:p-2 border rounded focus:border-pink-500" name="name" onChange={changeHandler} />
+          <input type="text" className="p-1 sm:p-2 border rounded focus-visible:border-green-400" name="name" onChange={changeHandler} />
         </div>
 
         <div className="flex flex-col mb-2 w-full md:w-2/4">
           <label className="capitalize font-medium mr-3">price</label>
-          <input type="number" className="p-1 sm:p-2  border rounded" name="price" onChange={changeHandler} />
+          <input type="number" className="p-1 sm:p-2  border rounded focus-visible:border-green-400" name="price" onChange={changeHandler} />
         </div>
       </div>
 
       <div className="flex flex-col md:flex-row justify-center md:justify-between items-center w-full mb-4">
         <div className="flex flex-col mb-2 w-full md:w-2/4 md:mr-4">
           <label className="capitalize font-medium mr-3">light</label>
-          <input type="text" className="p-1 sm:p-2  border rounded" name="light" onChange={changeHandler} />
+          <input type="text" className="p-1 sm:p-2  border rounded focus-visible:border-green-400" name="light" onChange={changeHandler} />
         </div>
 
         <div className="flex flex-col mb-2 w-full md:w-2/4">
           <label className="capitalize font-medium mr-3">irrigation</label>
-          <input type="text" className="p-1 sm:p-2  border rounded" name="irrigation" onChange={changeHandler} />
+          <input type="text" className="p-1 sm:p-2  border rounded focus-visible:border-green-400" name="irrigation" onChange={changeHandler} />
         </div>
       </div>
 
       <div className="flex flex-col mb-5 w-full">
         <label className="capitalize font-medium mr-3">explain</label>
-        <textarea className="w-full  p-2 border rounded" name="explain" rows={4} onChange={changeHandler} />
+        <textarea className="w-full  p-2 border roundedb focus-visible:border-green-400" name="explain" rows={4} onChange={changeHandler} />
       </div>
       
       <button type="submit" className="bg-green-600 hover:bg-green-700 transition ease-linear text-white font-medium px-1 py-2 rounded w-full" onClick={postCommentHandler}>
